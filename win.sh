c4d3c9f3a9eee8e745967647e517c2bb81fb615a
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-06-17 21:16:20 +0530

    Create win.sh

---
+++ win.sh
@@ -0,0 +1,12 @@
+apt update
+apt upgrade
+apt install git
+gem install lolcat
+pip2 install requests
+apt install figlet
+apt install wget
+cd
+cd windows-enemy
+cp -r white.exe /sdcard/
+figlet Prince Yasin |lolcat|lolcat
+echo -e "\e[1;34m[*] \e[32VVirus Created successfully with name of white.exe check your internal storage....\e[0m";
